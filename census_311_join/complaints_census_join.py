40de37efefd0e41daf59f1b27d1b5bbe2ef3befe
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2020-12-05 17:35:34 -0500

    "Add notebook with python version of census join"

--- census_311_join/complaints_census_join.py
+++ census_311_join/complaints_census_join.py
@@ -6,8 +6,8 @@ from pyspark import SparkContext
 from csv import reader
 
 def formatResult(row):
-  
-  formatted = '%s,%s,%s' % ( row[0], row[1][0], row[1][1] )  
+
+  formatted = '%s,%s,%s' % ( row[0], row[1][0], row[1][1] )
 
   return formatted
 
@@ -29,26 +29,27 @@ def formatComplaintsTuple(row):
 if __name__ == '__main__':
 
   sc = SparkContext()
-  
+
   ## reading census
   census = sc.textFile(sys.argv[1], 1)
-  census = census.mapPartitions(lambda x: reader(x)) 
-  
+  census = census.mapPartitions(lambda x: reader(x))
+
   ## census formatting
   census = census.map( lambda row: formatCensusTuple(row) )
 
-  
+
   ## reading complaints
   complaints = sc.textFile(sys.argv[2], 1)
-  complaints = complaints.mapPartitions(lambda x: reader(x)) 
+  complaints = complaints.mapPartitions(lambda x: reader(x))
 
   ## complaints formatting
   complaints = complaints.map( lambda row: formatComplaintsTuple(row))
 
   ## joining
   result = complaints.join(census)
-  ##result = result.sortBy(lambda x:  '_'.join( x[0].split('_')[0:3] + x[0].split('_')[3:4]  ), ascending=True) 
+  ##result = result.sortBy(lambda x:  '_'.join( x[0].split('_')[0:3] + x[0].split('_')[3:4]  ), ascending=True)
   result = result.map( lambda row: formatResult(row) )
 
-  result.saveAsTextFile('/user/jr4964/final-project/complaint_census_join.out')
+  output_path = sys.argv[3]
+  result.saveAsTextFile(output_path)
   sc.stop()
