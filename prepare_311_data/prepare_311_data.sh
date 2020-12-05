52191e2e5b90ee44c1ebec01890f04cbfab40945
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2020-12-04 19:13:51 -0500

    "Change output to script variable for timing the code"

--- prepare_311_data/prepare_311_data.sh
+++ prepare_311_data/prepare_311_data.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+# Used in the pipeline_compare scripts
+if [ $# -eq 0 ]
+	then
+		OUTPUT_PATH="/user/djk525/big-data/project/data/311_reduced.csv"
+		LOCAL_PATH="311_reduced.csv"
+
+	else
+		OUTPUT_PATH=$1
+		LOCAL_PATH="311_reduced_pipeline_cmp.csv"
+fi
+
 
 spark-submit --conf spark.pyspark.python=/share/apps/python/3.6.5/bin/python \
     prepare_311_data.py \
     /user/jr4964/final-project/complaints.csv \
     /user/djk525/big-data/project/data/zip_zcta.csv \
-    /user/djk525/big-data/project/data/311_reduced.csv \
+    $OUTPUT_PATH \
 
-/usr/bin/hadoop fs -getmerge big-data/project/data/311_reduced.csv 311_reduced.csv
+/usr/bin/hadoop fs -getmerge $OUTPUT_PATH $LOCAL_PATH
 
 # add header
-sed -i '1s/^/date,type,descriptor,zcta\n/' 311_reduced.csv
+sed -i '1s/^/date,type,descriptor,zcta\n/' $LOCAL_PATH
 
 # remove .crc file to prevent checksum error when putting file back to hfs
-rm .311_reduced.csv.crc 
+rm $LOCAL_PATH".crc"
 
-/usr/bin/hadoop fs -rm -r big-data/project/data/311_reduced.csv
-/usr/bin/hadoop fs -put 311_reduced.csv big-data/project/data/311_reduced.csv
+/usr/bin/hadoop fs -rm -r $OUTPUT_PATH
+/usr/bin/hadoop fs -put $LOCAL_PATH $OUTPUT_PATH
+
